twingl/trailblazer-extension

commit a8dd60863bf9acd338ad228e0f1859b9d02d0c78

setclass on open nodes, style changes for the same

core/state_manager.js
@@ -123,7 +123,7 @@
    * @param {number} assignmentId - The Assignment ID to scope the nodes to
    * @returns {Object}
    */
-  context.StateManager.prototype.getMap = function(assignmentId) {
+  context.StateManager.prototype.getMap = function(assignmentId, callback) {
     var data = {
       nodes: {}
     };
@@ -135,14 +135,27 @@
     // Form a Map<Node.id, Node>
     _.each(tmp, function(node, key) { data.nodes[node.id] = node; });
 
-    // If any tabs are open, set them as properties on the nodes
-    _.each(this._tabIdMap, function(map, key) {
-      if (data.nodes[map]) {
-        data.nodes[map].openTab = key;
-      }
-    });
+    chrome.tabs.query({currentWindow: true}, function(tabs) {
+      // two(!) methods to ensure opentabs are emphasised in UI 
+      //get currently open tab urls
+      var tabUrls = _.pluck(tabs, 'url');
+      _.each(data.nodes, function(node, id) {
+        var index = tabUrls.indexOf(node.url);
+        if (index > -1) {
+        //if node url matches open tab url set the node.tabId property
+          data.nodes[id].tabId = tabs[index].id;
+        };
+      });
 
-    return data;
+      // If any tabs are open as shown in tabIdMap, set them as properties on the nodes
+      _.each(this._tabIdMap, function(map, key) {
+        if (data.nodes[map]) {
+          data.nodes[map].tabId = key;
+        }
+      });
+
+      return callback(data);
+    });
   };
 
   /**
@@ -387,14 +400,14 @@
     if (this._tabIdMap[evt.data.tabId]) {
       // This is a resumed node
       node = Node.cache.read(this._storageAdapter, this._tabIdMap[evt.data.tabId]);
-      node.openTab = evt.data.tabId;
+      node.tabId = evt.data.tabId;
       this._tabIdMap[evt.data.tabId] = node.id;
     } else {
       // This is a new node
       node = new Node({
         url: evt.data.url,
         title: evt.data.title,
-        openTab: evt.data.tabId
+        tabId: evt.data.tabId
       });
 
       if (currentNode && evt.data.url !== "chrome://newtab/") {
@@ -403,7 +416,9 @@
         if (currentNode.recording && currentNode.assignmentId) {
           node.recording    = currentNode.recording;
           node.assignmentId = currentNode.assignmentId;
-          node.save(this._storageAdapter);
+          node.save(this._storageAdapter).then(function(savedNode) {
+            this._tabIdMap[evt.data.tabId] = savedNode.id;
+          }.bind(this));
         }
       }
 
@@ -413,10 +428,12 @@
         if (currentNode.recording && currentNode.assignmentId) {
           node.recording    = currentNode.recording;
           node.assignmentId = currentNode.assignmentId;
-          node.save(this._storageAdapter);
+
+          node.save(this._storageAdapter).then(function(savedNode) {
+            this._tabIdMap[evt.data.tabId] = savedNode.id;
+          }.bind(this));
         }
       }
-      this._tabIdMap[evt.data.tabId] = node.id;
     }
 
   };
@@ -440,18 +457,18 @@
       } else if (parentNode && evt.data.url && evt.data.url === parentNode.url) {
         // Navigating back
         var node = Node.cache.read(this._storageAdapter, node.id);
-        delete node.openTab;
+        delete node.tabId;
         this._tabIdMap[evt.data.tabId] = parentNode.id;
       } else if (Node.findWhere({ parentId: node.id, url: evt.data.url })) {
         // Navigating to an existing child
         var node = Node.cache.read(this._storageAdapter, node.id);
-        delete node.openTab;
+        delete node.tabId;
         this._tabIdMap[evt.data.tabId] = Node.findWhere({ parentId: node.id, url: evt.data.url }).id;
       } else {
         // Navigating to a new child
         var newNode = new Node({
           parentId:     node.id,
-          openTab:      node.openTab,
+          tabId:      node.tabId,
           url:          evt.data.url,
           title:        evt.data.title
         });
@@ -459,14 +476,15 @@
         if (node.recording) {
           newNode.recording    = node.recording;
           newNode.assignmentId = node.assignmentId;
-        }
+        };
 
         if (typeof node.id === "number" && newNode.recording && newNode.assignmentId) {
-          newNode.save(this._storageAdapter);
-        }
+          newNode.save(this._storageAdapter).then(function(savedNode) {
+            this._tabIdMap[evt.data.tabId] = savedNode.id;
+          }.bind(this));
+        };
 
-        delete node.openTab;
-        this._tabIdMap[evt.data.tabId] = newNode.id;
+        delete node.tabId;
       }
     } else if (node && evt.data.title) {
       node.title = evt.data.title;
@@ -496,7 +514,7 @@
   context.StateManager.prototype.closedTab = function(evt) {
     var node = Node.cache.read(this._storageAdapter, this._tabIdMap[evt.data.tabId]);
     delete node.recording;
-    delete node.openTab;
+    delete node.tabId;
     delete this._tabIdMap[evt.data.tabId];
   };
 

background.js
@@ -140,7 +140,11 @@
   chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
     switch (request.action) {
       case 'getMap': 
-        sendResponse({ data: stateManager.getMap(request.assignmentId) });
+        stateManager.getMap(request.assignmentId, function(data) {
+          sendResponse({data: data})
+
+        })
+        // sendResponse({ data: stateManager.getMap(request.assignmentId) });
         break;
 
       case 'getNodes':
@@ -170,7 +174,7 @@
        */
       case 'resumeAssignment':
         var node = Node.cache.read(stateManager._storageAdapter, request.nodeId);
-        chrome.tabs.create({ url: node.url }, function(tab) {
+        chrome.tabs.create({ url: node.url, active: false }, function(tab) {
           stateManager.resumeRecording(tab.id, request.nodeId);
         });
         break;

model/node.js
@@ -27,7 +27,7 @@
     this.recording    = properties.recording;
     this.url          = properties.url;
     this.title        = properties.title;
-    this.openTab      = properties.openTab;
+    this.tabId        = properties.tabId;
 
     context.Node._instances[this.id] = this;
   };

ui/pages/js/build/map.js
@@ -1,5 +1,11 @@
 (function() {
   'use strict';
+
+  var nodeCoreD = "M18.9063355 0.1 L33.942812 9 C34.6940305 9.5 35.3 10.5 35.3 11.4 L35.3030134 29.2 C35.3030134 30.1 34.7 31.2 33.9 31.6 L18.9063355 40.5 C18.1551171 40.9 16.9 40.9 16.2 40.5 L1.14945629 31.6 C0.39823781 31.2 -0.2 30.1 -0.2 29.2 L-0.21074509 11.4 C-0.21074509 10.5 0.4 9.5 1.1 9 L16.1859328 0.1 C16.9371513 -0.3 18.2 -0.3 18.9 0.1 Z",
+      nodeHaloD = "M18.2007195 11.2 L24.9141649 15.1 C25.2495669 15.3 25.5 15.8 25.5 16.2 L25.5214639 24.2 C25.5214639 24.5 25.2 25 24.9 25.2 L18.2007195 29.2 C17.8653175 29.4 17.3 29.4 17 29.2 L10.272676 25.2 C9.93727401 25 9.7 24.5 9.7 24.2 L9.66537697 16.2 C9.66537697 15.8 9.9 15.3 10.3 15.1 L16.9861214 11.2 C17.3215234 11 17.9 11 18.2 11.2 Z",
+      //hack to account for offset [0,0] coordinates of generated inline svg
+      offsetX = -18,
+      offsetY = -21;
   /**
    *  Render cleaned data
    */
@@ -92,10 +98,12 @@
           .call(d3.behavior.zoom().scaleExtent([.5, 4]).on("zoom", zoom))
         .append("g");
 
+/* This may do nothing - May influence the zooming function (labrador at chemistry set.jpg) */
     svg.append("rect")
       .attr("class", "overlay")
       .attr("width", width)
       .attr("height", height);
+/* This may do nothing - May influence the zooming function */
 
     function zoom() {
       svg.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
@@ -112,26 +120,16 @@
     var gnodes = svg.selectAll(".node")
           .data(data.nodes)
           .enter()
-            .append("svg:a")
-            .attr('xlink:href', function(d) { return d.url })
-          .append('g');
+          .append('g')
+          .attr('class', function(d) { return d.tabId ? 'open node' : 'closed node' });
 
-    var nodes = gnodes.append("circle")
-              .attr("class", "node")
-              .attr("r", 20);
+    var nodeHalos = gnodes.append('path')
+                    .attr('d', nodeHaloD)
+                    .attr("class", "node-core");
 
-
-/** Kept as reference, Moving styling to style.css
-              .style("fill", function(d) {
-                return ( d.parentId ? "rgba(0,225,0" : "rgba(0,0,255" )
-                  + ( d.recording ? ",1)" : ",0.1)" );
-              })
-              .style("stroke", "rgba(0,100,100,1)")
-              .style("stroke-width", function(d) {
-                return (d.openTab) ? "2" : "0";
-              });
-*/ 
-  
+    var nodes = gnodes.append('path')
+                  .attr('d', nodeCoreD)
+                  .attr("class", "node-halo");
 
     force.on("tick", function() {
       link.attr("x1", function(d) { return d.source.x; })
@@ -139,8 +137,8 @@
           .attr("x2", function(d) { return d.target.x; })
           .attr("y2", function(d) { return d.target.y; });
 
-      nodes.attr("cx", function(d) { return d.x; })
-          .attr("cy", function(d) { return d.y; });
+      gnodes.attr("transform", function(d) { return "translate(" +(d.x+offsetX)+ "," +(d.y+offsetY)+ ")"; })
+
     });
 
     svg.call(tip);
@@ -148,6 +146,22 @@
       .data(data.nodes)
       .on('mouseover', tip.show)
       .on('mouseout', tip.hide);
+
+    // Register the click handler for the nodes
+    svg.selectAll(".node")
+      .on('click', function(node) {
+        // If a link is middle clicked or ctrl/cmd+clicked
+        if (d3.event.which === 2 || (d3.event.which === 1 && (d3.event.metaKey || d3.event.ctrlKey))) {
+          d3.event.preventDefault();
+          d3.event.stopPropagation();
+ 
+          // Tell the runtime to open a new tab
+          chrome.runtime.sendMessage({
+            action: 'resumeAssignment',
+            nodeId: node.id
+          });
+        }
+      });
   };
 
   var assignmentId;

ui/pages/styles/map.css
@@ -14,7 +14,7 @@
 
 body {
 	font:400 13px/1.4 'Helvetica Neue', Helvetica, Arial, sans-serif;
-	background:#fff;
+	background:#5CC8CB;
 }
 
 html {
@@ -41,16 +41,28 @@ html {
 	margin: 0 auto;
 }
 
+/* nodes */
 .node {
 	fill: #fff;
-	stroke: #555;
-	stroke-width: 2px;
-	vector-effect: non-scaling-stroke;
+	fill-rule: evenodd;
+}
+.node-halo {
+	opacity: 0.1;
+}
+.node-core {
+	opacity: 0.9;
 }
 
+.open .node-halo{
+	opacity: 0.8;
+
+}
+
+/* links */
 .link {
-	stroke: #444;
-	stroke-width: 2px;
+	stroke: #fff;
+	stroke-width: 1px;
+	opacity: .2;
 	vector-effect: non-scaling-stroke;
 }
 
@@ -59,13 +71,7 @@ html {
   pointer-events: all;
 }
 
-
-
-
-
-
-
-
+/* dropdown */
 
 .click-nav {
 	margin:30px auto;

ui/popup/not_authenticated/script.js
@@ -1,123 +1,0 @@
-(function() {
-  'use strict';
-  /**
-   *  Render cleaned data
-   */
-  // Convert our data structure into one that d3 can render
-  var d3ify = function(data) {
-    console.log(data);
-    // Initialize the final containers that will be given to D3 to render
-    var links = [];
-    var nodes = [];
-    
-    // We need to convert our trees into a list of nodes and links.
-    // Keep track of the index so that we can associate the array of nodes/links
-    // with their respective locations in the original object
-    var index = 0;
-    var idMap = {};
-    
-    // Iterate over each tree and each tree's nodes
-    _.each(data.nodes, function(node, nodeId) {
-      // Record the relationship between index and location in the original
-      // object
-      idMap[nodeId] = {
-        index: index++,
-        treeId: node.treeId,
-        nodeId: nodeId
-      };
-      
-      // Add the node to the rendering list of nodes
-      nodes.push(node);
-      
-      // Add a link to the rendering list if there's a relationship present.
-      // Note the directionality: parentNode -> node
-      if (node.parentId) {
-        links.push({
-          source: idMap[node.parentId].index,
-          target: idMap[node.id].index
-        });
-      }
-    });
-    
-    return {
-      nodes: nodes,
-      links: links
-    };
-  };
-  
-  var syntaxHighlight = function(json) {
-    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-      var cls = 'number';
-      if (/^"/.test(match)) {
-        if (/:$/.test(match)) {
-          cls = 'key';
-        } else {
-          cls = 'string';
-        }
-      } else if (/true|false/.test(match)) {
-        cls = 'boolean';
-      } else if (/null/.test(match)) {
-        cls = 'null';
-      }
-      return '<span class="' + cls + '">' + match + '</span>';
-    });
-  };
-
-  // Rendering function - takes the format returned by d3ify(), i.e.
-  // render("selector", {nodes: nodes[], links: links[] })
-  var render = function(selector, data) {
-    var tip = d3.tip()
-      .direction("e")
-      .offset([0, 10])
-      .attr("class", "d3-tip")
-      .html(function(d) {
-        return "<pre>" + syntaxHighlight(JSON.stringify(d, null, 3)) + "</pre>";
-      });
-    var force = d3.layout.force().size([400,300]);
-    
-    var svg = d3.select(selector).append("svg")
-          .attr("width", 400)
-          .attr("height", 300);
-          
-    force.nodes(data.nodes).links(data.links).start();
-    
-    var link = svg.selectAll(".link")
-          .data(data.links)
-          .enter()
-            .append("line")
-              .attr("class", "link")
-              .style("stroke", "#555")
-              .style("stroke-width", 2);
-  
-    var node = svg.selectAll(".node")
-          .data(data.nodes)
-          .enter()
-            .append("circle")
-              .attr("class", "node")
-              .attr("r", 5)
-              .style("fill", function(d) { return d.parentId ? "red" : "blue"; });
-              
-    force.on("tick", function() {
-      link.attr("x1", function(d) { return d.source.x; })
-          .attr("y1", function(d) { return d.source.y; })
-          .attr("x2", function(d) { return d.target.x; })
-          .attr("y2", function(d) { return d.target.y; });
-  
-      node.attr("cx", function(d) { return d.x; })
-          .attr("cy", function(d) { return d.y; });
-    });
-    
-    svg.call(tip);
-    svg.selectAll(".node")
-      .data(data.nodes)
-      .on('mouseover', tip.show)
-      .on('mouseout', tip.hide);
-  };
-
-  chrome.runtime.sendMessage({ action: "getMap", assignmentId: assignmentId }, function(response) {
-      if (response.data && response.data.nodes && Object.keys(response.data.nodes).length > 0) {
-        render("#map", d3ify( response.data ));
-      };
-    });
-}());

commit 2ccd5645d2a9053ac1083476dd428e306ca7667d

Map tabIds to temporary IDs
Regression introduced in a8dd60863

core/state_manager.js
@@ -434,6 +434,9 @@
           }.bind(this));
         }
       }
+
+      // Map to the temporary ID, this will be overwritten if the node is saved
+      this._tabIdMap[evt.data.tabId] = node.id;
     }
 
   };
@@ -484,6 +487,8 @@
           }.bind(this));
         };
 
+        // Map to the temporary ID, this will be overwritten if the node is saved
+        this._tabIdMap[evt.data.tabId] = newNode.id;
         delete node.tabId;
       }
     } else if (node && evt.data.title) {