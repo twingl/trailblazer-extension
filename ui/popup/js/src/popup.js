1bf9e1144ff07f46a458eccc318e06dcf937454f
Simon Tegg <simon@loomio.org>
2014-09-26 16:42:19 +1200

    react component

--- ui/popup/js/src/popup.js
+++ ui/popup/js/src/popup.js
@@ -1,3 +1,11 @@
 var React = require('react');
 var TrailTitle = require('app/components/trail-title');
 
+chrome.runtime.sendMessage({action: 'getCurrentAssignment'}, function(response) {
+  console.log('trail', response)
+})
+
+React.renderComponent(
+  <TrailTitle value={"test"} />,
+  document.getElementById('recording-title'));
+
