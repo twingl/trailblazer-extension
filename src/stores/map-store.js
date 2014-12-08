861a504b4f1c0bcd55c5e0c841145aae6970b3ac
Greg Signal <greg@awesome.org.nz>
2014-12-08 15:07:59 +1300

    Tidy up MapStore

--- src/stores/map-store.js
+++ src/stores/map-store.js
@@ -1,8 +1,6 @@
 var _                             = require('lodash')
   , Fluxxor                       = require('fluxxor')
   , constants                     = require('../constants')
-  , TrailblazerHTTPStorageAdapter = require('../adapter/trailblazer_http_storage_adapter')
-  , camelize                      = require('camelcase-keys')
   , info                          = require('debug')('stores/map-store.js:info')
   , warn                          = require('debug')('stores/map-store.js:warn')
   , error                         = require('debug')('stores/map-store.js:error');
@@ -19,13 +17,6 @@ var MapStore = Fluxxor.createStore({
     this.error              = null;
 
     this.bindActions(
-//      constants.FETCH_ASSIGNMENTS, this.handleFetchAssignments,
-//      constants.FETCH_ASSIGNMENTS_SUCCESS, this.handleFetchAssignmentsSuccess,
-//      constants.FETCH_ASSIGNMENTS_FAIL, this.handleFetchAssignmentsFail,
-//      constants.UPDATE_ASSIGNMENT_CACHE, this.handleUpdateAssignmentCache,
-//      constants.UPDATE_ASSIGNMENT_CACHE_SUCCESS, this.handleUpdateAssignmentCacheSuccess,
-//      constants.UPDATE_ASSIGNMENT_CACHE_FAIL, this.handleUpdateAssignmentCacheFail,
-//      constants.ASSIGNMENTS_SYNCHRONIZED, this.handleAssignmentsSynchronized,
       constants.LOAD_NODES_SUCCESS, this.handleLoadNodesSuccess,
       constants.SELECT_ASSIGNMENT, this.handleSelectAssignment
     );
@@ -45,7 +36,7 @@ var MapStore = Fluxxor.createStore({
     error('db error', { error: err });
   },
 
-  handleDispatchNodes: function (data) {
+  handleDispatchNodes: function (data) { //TBD
     this.emit('change', { nodes: data });
   },
 
@@ -53,15 +44,15 @@ var MapStore = Fluxxor.createStore({
     warn('handleGetAllNodesFail', { error: error })
   },
 
-  handleLoadNodesSuccess: function (payload) {
+  handleLoadNodesSuccess: function (payload) { //TBD
     info('handleLoadNodesSuccess', { payload: payload });
-    this.waitFor(['NodeStore'], function (nodeStore) {
-      //TODO search by currentAssignment index
-      var nodes = nodeStore.getState().db.getAll(this.dispatchNodes, this.handleGetAllNodesFail)
+    this.waitFor(['NodeStore'], function (nodeStore) { //TBD
+      //TODO search by currentAssignment index //TBD
+      var nodes = nodeStore.getState().db.getAll(this.dispatchNodes, this.handleGetAllNodesFail) //TBD
     }.bind(this))
   },
 
-  handleSelectAssignment: function (payload) {
+  handleSelectAssignment: function (payload) { //TBD
     info('handleSelectAssignment', { payload: payload })
     this.currentAssignment = payload.assignmentId; //NO
   }
