dd2f50daf18edbbb0cacb89442fee6b6c5c53995
Greg Signal <greg@awesome.org.nz>
2014-12-20 17:52:19 +1300

    Remove browser-sync

--- gulp/config.js
+++ gulp/config.js
@@ -2,18 +2,6 @@ var dest = "./build";
 var src = './src';
 
 module.exports = {
-  // browserSync: {
-  //   server: {
-  //     // We're serving the src folder as well
-  //     // for sass sourcemap linking
-  //     baseDir: [dest, src]
-  //   },
-  //   files: [
-  //     dest + "/**",
-  //     // Exclude Map files
-  //     "!" + dest + "/**.map"
-  //   ]
-  // },
   sass: {
     src: src + "/content/styles/*.css",
     dest: dest
