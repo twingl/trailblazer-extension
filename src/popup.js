4959609ef0091ef8ebc90641b031644fbff4efa1
Greg Signal <greg@awesome.org.nz>
2015-01-09 19:59:11 +1300

    Specify that we aren't using pushstate

--- src/popup.js
+++ src/popup.js
@@ -7,8 +7,10 @@ var App = require('./popup/app');
 domready(function() {
   chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
     if (tabs[0]) {
-      React.render(
-        React.createElement(App, { tabId: tabs[0].id }), document.body);
+      React.render( React.createElement(App, {
+        tabId: tabs[0].id,
+        history: false
+      }), document.body);
     }
   });
 });
