a4b1ba4ff69c9debbd5dc00e1c47006eef3235d3
Greg Signal <greg@awesome.org.nz>
2015-02-14 17:44:12 +1300

    First design pass of the onboarding.
    - Favicons
    - Ring around root node on map
    - Page styling

--- src/tour/4.js
+++ src/tour/4.js
@@ -1,7 +1,10 @@
 var $ = require('jquery');
 
 $(document).ready( function () {
-  $('#btnClose').click( function () {
+  $('#btnClose').click( function (evt) {
+
+    evt.stopPropagation();
+
     chrome.tabs.getCurrent( function (tab) {
       chrome.tabs.remove(tab.id);
     });
