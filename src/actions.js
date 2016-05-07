twingl/trailblazer-extension

commit d56d0579e304e77b82d02b75923319c3e4de2197

Override message sender for actions from Cr events
This fixes an issue related to
https://bugs.chromium.org/p/chromium/issues/detail?id=479951 where we
were relying on Chrome.runtime to deliver messages within the same
context, which was apparently never the intended behaviour of that
feature in Chrome. The fact that this ever worked is inconvenient.

src/actions.js
@@ -1,10 +1,18 @@
 import constants      from './constants';
-import messageChannel from './util/message-channel';
+import { send as sendMessage, receive as receiveMessage } from './util/message-channel';
 
 import Logger from './util/logger';
 var logger = Logger('actions.js');
 
+const messageChannel = {
+  send: sendMessage,
+  receive: receiveMessage
+}
+
 export default {
+  setMessageSender: function(func) {
+    messageChannel.send = func;
+  },
 
   requestAssignments: function() {
     logger.info('requestAssignments');

src/background/chrome-events.js
@@ -1,53 +1,71 @@
 import actions from '../actions';
 
-chrome.tabs.onCreated.addListener(function(tab) {
-  actions.tabCreated(tab.id, tab.url, tab.title, tab.openerTabId, tab);
-});
+// Initialize our logger
+import Logger from '../util/logger';
+var logger = Logger('background/chrome-events.js');
 
-chrome.tabs.onActivated.addListener(function(focusInfo) {
-  actions.tabFocused(focusInfo.tabId);
-});
+export default function bind(flux) {
+  actions.setMessageSender(function(message) {
+    if (message.action) {
+      var e = {
+        type: message.action,
+        payload: message.payload || {}
+      }
+      flux.dispatcher.dispatch(e);
+      logger.info("Dispatched CrEvent", e);
+    }
+  })
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  // filter the update events, so only url/title etc. changes are fired
-  if (changeInfo.url) {
-    actions.tabUpdated(tabId, changeInfo.url, tab.title, tab);
-  }
-});
 
-chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
-  actions.tabClosed(tabId);
-});
+  chrome.tabs.onCreated.addListener(function(tab) {
+    actions.tabCreated(tab.id, tab.url, tab.title, tab.openerTabId, tab);
+  });
 
-chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
-  if (details.frameId === 0) {
-    actions.historyStateUpdated(
+  chrome.tabs.onActivated.addListener(function(focusInfo) {
+    actions.tabFocused(focusInfo.tabId);
+  });
+
+  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    // filter the update events, so only url/title etc. changes are fired
+    if (changeInfo.url) {
+      actions.tabUpdated(tabId, changeInfo.url, tab.title, tab);
+    }
+  });
+
+  chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+    actions.tabClosed(tabId);
+  });
+
+  chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+    if (details.frameId === 0) {
+      actions.historyStateUpdated(
+          details.tabId,
+          details.url,
+          details.transitionType,
+          details.transitionQualifiers,
+          details.timestamp);
+    }
+  });
+
+  chrome.tabs.onReplaced.addListener(function(addedTabId, removedTabId) {
+    actions.tabReplaced(addedTabId, removedTabId);
+  });
+
+  chrome.webNavigation.onCreatedNavigationTarget.addListener(function(details) {
+    actions.createdNavigationTarget(
+        details.sourceTabId,
         details.tabId,
         details.url,
-        details.transitionType,
-        details.transitionQualifiers,
         details.timestamp);
-  }
-});
+  });
 
-chrome.tabs.onReplaced.addListener(function(addedTabId, removedTabId) {
-  actions.tabReplaced(addedTabId, removedTabId);
-});
-
-chrome.webNavigation.onCreatedNavigationTarget.addListener(function(details) {
-  actions.createdNavigationTarget(
-      details.sourceTabId,
-      details.tabId,
-      details.url,
-      details.timestamp);
-});
-
-chrome.webNavigation.onCommitted.addListener(function(details) {
-  if (details.frameId === 0) {
-    actions.webNavCommitted(details.tabId,
-        details.url,
-        details.transitionType,
-        details.transitionQualifiers,
-        details.timestamp);
-  }
-});
+  chrome.webNavigation.onCommitted.addListener(function(details) {
+    if (details.frameId === 0) {
+      actions.webNavCommitted(details.tabId,
+          details.url,
+          details.transitionType,
+          details.transitionQualifiers,
+          details.timestamp);
+    }
+  });
+}

src/scripts/background.js
@@ -99,7 +99,8 @@ proxyChange(flux, [
 ]);
 
 // Wire up Chrome events to fire the appropriate actions
-import chromeEvents from '../background/chrome-events';
+import bindChromeEvents from '../background/chrome-events';
+bindChromeEvents(flux);
 
 // Inject content-scripts into pages
 import contentScripts from '../background/content-scripts';

commit 66abd6fce0c5bc9121a7f58c7700c9ff03c03a54

Fix background dispatches not working
Regression introduced in d56d057 which would override the dispatch for
all action senders, so now we dispatch the action and then call the
original message channel

src/actions.js
@@ -10,6 +10,10 @@ const messageChannel = {
 }
 
 export default {
+  getMessageSender: function() {
+    return messageChannel.send;
+  },
+
   setMessageSender: function(func) {
     messageChannel.send = func;
   },

src/background/chrome-events.js
@@ -5,18 +5,6 @@ import Logger from '../util/logger';
 var logger = Logger('background/chrome-events.js');
 
 export default function bind(flux) {
-  actions.setMessageSender(function(message) {
-    if (message.action) {
-      var e = {
-        type: message.action,
-        payload: message.payload || {}
-      }
-      flux.dispatcher.dispatch(e);
-      logger.info("Dispatched CrEvent", e);
-    }
-  })
-
-
   chrome.tabs.onCreated.addListener(function(tab) {
     actions.tabCreated(tab.id, tab.url, tab.title, tab.openerTabId, tab);
   });

src/scripts/background.js
@@ -57,6 +57,21 @@ import stores from '../stores';
 logger.info("Initializing Flux", { stores: stores, actions: actions });
 var flux = new Fluxxor.Flux(stores, actions);
 
+var send = actions.getMessageSender();
+actions.setMessageSender(function(message) {
+  if (message.action) {
+    var e = {
+      type: message.action,
+      payload: message.payload || {}
+    }
+    flux.dispatcher.dispatch(e);
+    logger.info("Dispatched Background Event", e);
+  }
+
+  // Send through old channel as well
+  send(message);
+});
+
 _.each(stores, (s) => {
   console.log(s.onBoot, s);
   s.onBoot();
@@ -81,8 +96,11 @@ chrome.runtime.onMessage.addListener(function(message, sender, responder) {
     // TODO: query method currently needs to respond with a promise. This
     // shouldn't be a requirement, or at least it shouldn't present itself in
     // the source: synchronous methods should work seamlessly.
-    flux.store(message.store)[message.query](...message.args).then(responder);
-    logger.info("Queried", message);
+    flux.store(message.store)[message.query](...message.args).then(res => {
+      logger.info(`Query response: ${message.store}.${message.query}`, res);
+      responder(res);
+    });
+    logger.info(`Query: ${message.store}.${message.query}`, message);
     return true;
   };
 });